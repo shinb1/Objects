6c2c9585edeb3b640d715248b1cb63970f07ff38
shinb1 <allenmx@s.dcsdk12.org>
2013-10-10 09:24:41 -0600

    ATM pushed

---
+++ ATM.java
@@ -0,0 +1,37 @@
+public class ATM {
+	
+
+	private Account[] users = new Account[];
+
+
+	public ATM() {
+
+		users[0] = new Account["Bob", 1234, 100];
+		users[1] = new Account["Alice", 3333, 250];
+		users[2] = new Account["JT", 0001, 999999999];
+
+
+	}
+
+	public boolean login(String user, int pin) {
+
+
+
+	}
+
+
+
+
+}
+
+// ATM
+
+// State
+// Accounts
+
+
+// Behavior
+// Login
+// Withdraw
+// Check
+// Change Name
